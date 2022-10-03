497c388ad99aa78e1476121324578f868bed09ed
annagracecalhoun <annagracecalhoun@gmail.com>
2022-10-02 22:36:36 -0400

    create twitter skeleton

--- week3/twitter-app/src/App.js
+++ week3/twitter-app/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Header from "./Components/Header.js";
 import Body from "./Components/Body.js";
-import Blog from "./Components/Blog.js";
 
 const App = () => {
   return <div>
+
     <div><Header/></div>
-    <div><Body>
-      </Body></div>
-      <Blog title="My first blog" content="loving react so far" likes="100"></Blog>
+    <div><Body/></div>
+
   </div>;
 };
 
---
+++ week3/twitter-app/src/Components/Body.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Tweet from "./Tweet.js";
+
+
+const Body = () => {
+  return (
+    <div>
+      <h6>This is the body component!!</h6>
+      <Tweet handle="@jack" author="jack" content="loving react so far" likes="100"></Tweet>
+      <Tweet handle="@obama" author="obama" content="register to vote!!" likes="4000"></Tweet>
+    </div>
+  );
+};
+
+export default Body;
