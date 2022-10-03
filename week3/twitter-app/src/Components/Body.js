1750936e64fe07c31975c913279b92a9dc828133
annagracecalhoun <annagracecalhoun@gmail.com>
2022-10-03 10:46:21 -0400

    update props

--- week3/twitter-app/src/Components/Body.js
+++ week3/twitter-app/src/Components/Body.js
@@ -6,8 +6,8 @@ const Body = () => {
   return (
     <div>
       <h6>This is the body component!!</h6>
-      <Tweet handle="@jack" author="jack" content="loving react so far" likes="100"></Tweet>
-      <Tweet handle="@obama" author="obama" content="register to vote!!" likes="4000"></Tweet>
+      <Tweet handle={"@jack"} author={"jack"} content={"loving react so far"} likes={100}></Tweet>
+      <Tweet handle={"@obama"} author={"obama"} content={"register to vote!!"} likes={4000}></Tweet>
     </div>
   );
 };
