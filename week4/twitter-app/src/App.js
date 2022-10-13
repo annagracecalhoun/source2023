66832b1863ba6d050ddea7b082daaa7aefc24e76
annagracecalhoun <annagracecalhoun@gmail.com>
2022-10-13 13:00:41 -0400

    add week4 version

---
+++ week4/twitter-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Components/Header.js";
+import Body from "./Components/Body.js";
+
+const App = () => {
+  return <div>
+
+    <div><Header/></div>
+    <div><Body/></div>
+
+  </div>;
+};
+
+export default App;
