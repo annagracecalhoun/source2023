1e84c773dce09a21aedb2075060050b36b93b596
annagracecalhoun <annagracecalhoun@gmail.com>
2023-04-06 09:04:01 -0400

    Update App.js

--- week5/input/src/App.js
+++ week5/input/src/App.js
@@ -6,25 +6,28 @@ function App() {
   const [bio, setBio] = useState(""); 
   const [people, setPeople] = useState([]); 
 
-  const submitForm = () => {
-    setPeople([...people, {firstName: firstName, bio: bio}]); 
+  const submitPerson = () => {
+    setPeople([...people, {firstName:firstName, bio:bio}])
     setFirstName(""); 
     setBio(""); 
   }
 
   return (
+
     <div style={{ textAlign: 'center' }}>
       <h2>Input App</h2>
       <br></br>
-      <input value={firstName} onChange={e => setFirstName(e.target.value)} placeholder="First Name"/>
-      <br></br>
-      <input value={bio} onChange={e => setBio(e.target.value)} placeholder="Bio"/>
-      <br></br>
-      <button onClick={submitForm}>Submit</button>
 
-    <br></br><br></br>
-    {people.map((person, i) => 
-    (<h3>{person.firstName}, {person.bio}</h3>))}
+
+      <input onChange={(e) => setFirstName(e.target.value)} value={firstName} placeholder="Name"></input>
+      <br></br>
+      <input onChange={(e) => setBio(e.target.value)} value={bio} placeholder="Bio"></input>
+      <br></br>
+      <button onClick={submitPerson}>Submit</button>
+
+      {people.map((person) => (
+        <p>{person.firstName}: {person.bio}</p>
+      ))}
 
     </div>
   );
